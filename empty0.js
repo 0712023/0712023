7a07c6ee017d7d0f33ca6450f4d4f6490d67e4be
0712023 <0712023@hanmail.net>
2020-12-13 20:37:29 +0900

    test

--- empty0.js
+++ empty0.js
@@ -1,40 +1,6 @@
 function solution(n) {
-  var answer = 0;
-  var a = n;
-  var b = 1;
-  while (a>=1){
-    if(a%2==0){
-      a/=2;
-      b*=2;
-    } else{
-      break;
-    }
-    if(a == 1){
-      return 1;
-    }
+  for(var i = 2; i<(n+1)/2;i++){
+    
   }
-  if(b==1){
-
-  }
-  console.log(a, b);
-  answer+=check(a,b);
   return answer;
-}
-
-function hol(a, b){
-  if((a-1)/2-b>=0){
-    return 1;
-  } else{
-    return 0;
-  }
-}
-function zak(a, b){
-  if(b-(a-1)/2>0){
-    return 1;
-  } else {
-    return 0;
-  }
-}
-function check(a,b){
-  return hol(a,b)+zak(a,b);
 }
--- empty1.js
+++ empty1.js
@@ -1,4 +1,38 @@
 function solution(n) {
-  
-  return answer;
+  var answer = 0;
+  var a = [];
+  var b = n;
+  loop1:
+  for(var i=2;i<n/2;i++){
+    var c = [i, 0];
+    loop2:
+    while(b>=1){
+      if(b%i ==0){
+        b/=i;
+        c[1]++;
+      } else {
+        break loop2;
+      }
+    }
+    if(c[1]!=0){
+      a.push(c);
+    }
+  }
+  console.log(a);
+
+  if(a[0][0] !=2){
+    
+  }
+  return 0;
+}
+
+function check(a,b){
+  var c = 0;
+  if((a-1)/2-b>=0){
+    c++;
+  }
+  if(b-(a-1)/2>0){
+    c++;
+  }
+  return c;
 }
