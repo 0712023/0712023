8b8b0f952c3ed881e052d6244d93764585e9e060
0712023 <0712023@hanmail.net>
2020-12-31 19:49:04 +0900

    commit

--- Cyber_Tech_Friend_Freelancer/Useful Code/convert/convert uni to hex.js
+++
@@ -1,8 +0,0 @@
-function u2h(uni){
-  var resultlist = [] ;
-  for (i = 0; i<uni.length; i++) { //uni 길이만큼 반복
-    hex = uni.charCodeAt(i).toString(16) ; //uni에서 i번째 문자를 16진법으로 변환하여 hex에 지정
-    resultlist.push(("0x"+hex)) ;//hex값을 resultlist에 저장
-  }
-  return resultlist ;
-}
